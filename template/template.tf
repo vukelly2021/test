a4a407f274a0b009ea4d082a8ca5cffcb23a5164
vukelly2021 <84043745+vukelly2021@users.noreply.github.com>
2021-05-12 08:49:43 -0700

    Delete template directory

--- template/template.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket          = "tf-state"
-    key             = "usor/ProjectId.tfstate"
-    vpc             = "vpcId"
-    dynamodb_table  = "cfgs-usor-tf-lock"
-  }
-}
